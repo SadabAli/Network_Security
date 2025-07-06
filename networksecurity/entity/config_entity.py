3cd367e3117d52ec9509c1e963cf0f737424afb4
SadabAli <alisadab404@gmail.com>
2025-07-06 17:28:37 +0530

    model creation & Evaluation

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -61,14 +61,14 @@ class DataTransformationConfig:
         self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_piprline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
             training_piprline.PREPROCESSING_OBJECT_FILE_NAME,)
         
-# class ModelTrainerConfig:
-#     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-#         self.model_trainer_dir: str = os.path.join(
-#             training_pipeline_config.artifact_dir, training_piprline.MODEL_TRAINER_DIR_NAME
-#         )
-#         self.trained_model_file_path: str = os.path.join(
-#             self.model_trainer_dir, training_piprline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
-#             training_piprline.MODEL_FILE_NAME
-#         )
-#         self.expected_accuracy: float = training_piprline.MODEL_TRAINER_EXPECTED_SCORE
-#         self.overfitting_underfitting_threshold = training_piprline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_piprline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path: str = os.path.join(
+            self.model_trainer_dir, training_piprline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
+            training_piprline.MODEL_FILE_NAME
+        )
+        self.expected_accuracy: float = training_piprline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = training_piprline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
